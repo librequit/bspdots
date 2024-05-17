2f7099fe85fb3234ddbd52b030e952c28e4d13bb
librequit <141883843+librequit@users.noreply.github.com>
2024-05-17 14:00:25 +0300

    Add files via upload

--- cfg/bspwm/scripts/monitor.sh
+++ cfg/bspwm/scripts/monitor.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-D-0 --off --output HDMI-0 --primary --mode 1920x1080 --pos 0x420 --rotate normal --output DP-0 --off --output DP-1 --off --output None-2-1 --off --output DP-1-1 --mode 1920x1080 --pos 1920x0 --rotate left --output HDMI-1-1 --off --output HDMI-1-2 --off
+xrandr --output DVI-D-0 --off --output HDMI-0 --primary --mode 1920x1080 --rate 75.00 --pos 0x420 --rotate normal --output DP-0 --off --output DP-1 --off --output None-2-1 --off --output DP-1-1 --mode 1920x1080 --pos 1920x0 --rotate left --output HDMI-1-1 --off --output HDMI-1-2 --off
